ea86c8b9a541c939b8c49bb4f113b4c8b58191e1
NurRizqiMaulana <maulana29.rizqi@gmail.com>
2021-12-17 01:50:10 +0700

    minggu4

---
+++ index.js
@@ -0,0 +1,29 @@
+// var nilai;
+var nilairaya = 80;
+var nilaiGenta = 50;
+
+if (nilairaya >= 80) {
+  alert("indeksnya adalah A");
+} else if (nilairaya && nilairaya < 80) {
+  alert("indeksnya adalah B");
+} else if (nilairaya  >= 60 && nilairaya  < 70) {
+  alert("indeksnya adalah C");
+} else if (nilairaya  > 50 && nilairaya  <= 60) {
+  alert("indeksnya adalah D");
+} else {
+  alert("indeksnya adalah E");
+}
+
+// var nilaiGenta = 50;
+
+if (nilaiGenta >= 80) {
+  alert("indeksnya adalah A");
+} else if ( nilaiGenta >= 70 && nilaiGenta < 80) {
+  alert("indeksnya adalah B");
+} else if (nilaiGenta >= 60 && nilaiGenta < 70) {
+  alert("indeksnya adalah C");
+} else if (nilaiGenta >= 50 && nilaiGenta < 60) {
+  alert("indeksnya adalah D");
+} else {
+  alert("indeksnya adalah E");
+}
